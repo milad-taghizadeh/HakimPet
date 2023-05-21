a150f8454bc77b343b143bcbc33ad8dd004ba629
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-05-21 19:00:20 +0330

    commit 2.0

---
+++ js/contact.js
@@ -0,0 +1,19 @@
+//index.js  
+var emailText = document.getElementById("#email");
+console.log(emailText);
+
+function sendEmail() {
+	Email.send({
+	Host: "smtp.gmail.com",
+	Username : "taghizadehmilad1382@gmail.com",
+	Password : "mt8289milad",
+	To : 'miladtaghizadeh1382@outlook.com',
+	From : "taghizadehmilad1382@gmail.com",
+	Subject : "test message",
+	Body : "test",
+	}).then(
+		message => alert("mail sent successfully")
+	);
+}
+document.addEventListener("click", sendEmail());
+
---
+++ js/footerSlider.js
@@ -0,0 +1,6 @@
+var swiper = new Swiper(".mySwiperFooter", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+},
+});
