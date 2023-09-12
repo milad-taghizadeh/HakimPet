973074322eef1a13e6e8033e4f3a7d838541bca3
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-12 23:13:26 +0330

    AdminPanel-MessageBox built successfully

--- js/admin.js
+++ js/admin.js
@@ -1,4 +1,3 @@
-javascript
 function showMessages() {
     hideAllSections();
     document.getElementById("messages").style.display = "block";
@@ -28,3 +27,24 @@ function hideAllSections() {
 
 // Show the dashboard section by default
 showMessages();
+
+
+// MessagesBox
+fetch('http://localhost:3000/api/v0/massages/')
+    .then(response => response.json())
+    .then(data => {
+        let messageBoxdata = ``;
+        for (let i = 0; i < Object.keys(data).length; i++) {
+            const key = i.toString();
+            messageBoxdata += `<div class="frame">
+            <div class="name"><span>نام : </span></span><p id="Name">${data[key].name}</p></div>
+            <div class="email"><span>آدرس ایمیل : </span></span><p id="Email">${data[key].email}</p></div>
+            <div class="message"><span>پیام : </span></span><p id="Message">${data[key].text}</p></div>
+            </div>`;
+        }
+        document.getElementById('messageBox').innerHTML = messageBoxdata;
+    })
+    // .then(data => console.log(data))
+    .catch(error => {
+        console.error(error);
+    });
