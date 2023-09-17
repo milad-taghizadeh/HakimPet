73a0deb36dd211f5b2e646d393a21559283eb906
miladtaghizadeh1382 <miladtaghizadeh1382@outlook.com>
2023-09-18 03:13:45 +0330

    update:vaccineBox built with some bugs that not fixed

--- js/admin.js
+++ js/admin.js
@@ -73,7 +73,7 @@ fetch('http://localhost:3000/api/v0/SV/')
 
                 // Your logic here using svData and userDataArray
                 let sendVetData = ``;
-                for (let i = Object.keys(svData , userDataArray).length - 1; i >= 0; i--) {
+                for (let i = Object.keys(svData, userDataArray).length - 1; i >= 0; i--) {
                     const key = i.toString();
                     sendVetData += `<div class="frame" id="frame">
             <div class="name"><span>نام : </span></span>
@@ -103,50 +103,60 @@ fetch('http://localhost:3000/api/v0/SV/')
         console.error(error);
     });
 
-    // fetch('http://localhost:3000/api/v0/SV/')
-    // .then(response => response.json())
-    // .then(svData => {
-    //     // Get the ID from the first URL
-    //     for (let i = Object.keys(svData).length - 1; i >= 0; i--) {
-    //         const key = i.toString();
-    //         const id = svData[key].userId;
-    //         const userURL = `http://localhost:3000/api/v0/user/find/${id}`;
-        
-    //     return fetch(userURL, { credentials: 'include' })
-    //         .then(response => response.json())
-    //         .then(userData => {
-    //             console.log(svData); // Access svData
-    //             console.log(userData); // Access userData
+// Vaccine Box
+fetch('http://localhost:3000/api/v0/vaccination/')
+    .then(response => response.json())
+    .then(vaccineData => {
+        // Get the ID from the first URL
+        const promises = [];
+        for (let i = Object.keys(vaccineData).length - 1; i >= 0; i--) {
+            const key = i.toString();
+            const id = vaccineData[key].userId;
+            const userURL = `http://localhost:3000/api/v0/user/find/${id}`;
 
-    //             // Your logic here using svData and userData
-    //             let sendVetData = ``;
-    //             for (let i = Object.keys(svData).length - 1; i >= 0; i--) {
-    //                 const key = i.toString();
-    //                 sendVetData += `<div class="frame" id="frame">
-    //         <div class="name"><span>نام : </span></span>
-    //         <p id="Name">${userData.name}</p>
-    //         </div>
-    //         <div class="email"><span>آدرس ایمیل : </span></span>
-    //         <p id="Email">${userData.email}</p>
-    //         </div>
-    //         <div class="address"><span>آدرس : </span></span>
-    //         <p id="Address">${userData.address}</p>
-    //         </div>
-    //         <div class="number"><span>شماره تلفن : </span></span>
-    //         <p id="Number">${userData.phoneNumber}</p>
-    //         </div>
-    //         <div class="pet"><span>حیوان : </span></span>
-    //         <p id="Pet">${userData.pet}</p>
-    //         </div>
-    //         <div class="sendVetDate"><span>تاریخ درخواستی : </span></span>
-    //         <p id="SendVetDate">${svData[key].date}</p>
-    //         </div>
-    //       </div>`;
-    //             }
-    //             document.getElementById('sendVetBox').innerHTML = sendVetData;
-    //         });
-    //     }
-    // })
-    // .catch(error => {
-    //     console.error(error);
-    // });
+            // Fetch user data for each ID
+            const promise = fetch(userURL, { credentials: 'include' })
+                .then(response => response.json());
+            promises.push(promise);
+        }
+
+        // Wait for all promises to resolve
+        return Promise.all(promises)
+            .then(userDataArray => {
+                console.log(vaccineData); // Access svData
+                console.log(userDataArray); // Access userDataArray
+
+                // Your logic here using svData and userDataArray
+                let vcData = ``;
+                for (let i = Object.keys(vaccineData, userDataArray).length - 1; i >= 0; i--) {
+                    const key = i.toString();
+                    vcData += `<div class="frame">
+                    <div class="name"><span>نام : </span></span>
+                        <p id="Name">${userDataArray[key].name}</p>
+                    </div>
+                    <div class="email"><span>آدرس ایمیل : </span></span>
+                        <p id="Email">${userDataArray[key].email}</p>
+                    </div>
+                    <div class="address"><span>آدرس : </span></span>
+                        <p id="Address">${userDataArray[key].address}</p>
+                    </div>
+                    <div class="number"><span>شماره تلفن : </span></span>
+                        <p id="Number">${userDataArray[key].phoneNumber}</p>
+                    </div>
+                    <div class="pet"><span>حیوان : </span></span>
+                        <p id="Pet">${userDataArray[key].pet}</p>
+                    </div>
+                    <div class="vaccineDate"><span>تاریخ واکسن قبلی : </span></span>
+                        <p id="VaccineDate">${vaccineData[key].lastVaccineDate}</p>
+                    </div>
+                    <div class="vaccineType"><span>نوع واکسن درخواستی : </span></span>
+                        <p id="VaccineType">${vaccineData[key].vaccineName}</p>
+                    </div>
+                </div>`;
+                }
+                document.getElementById('vaccineBox').innerHTML = vcData;
+            });
+    })
+    .catch(error => {
+        console.error(error);
+    });
